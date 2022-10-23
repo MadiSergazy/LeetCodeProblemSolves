c7fc1a5247429ac5c5d39159da35bdd2f63a29a4
MadiSergazy <113364296+MadiSergazy@users.noreply.github.com>
2022-10-23 10:19:54 +0600

    Time: 59 ms (6.84%), Space: 4.8 MB (29.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -0,0 +1,16 @@
+
+      
+func isPalindrome(x int) bool {
+    strX := strconv.Itoa(x)
+    masR := []rune(strX)
+    
+    for i, j := 0, len(masR)-1; i < j; i, j = i+1, j-1{
+        if !(masR[i] == masR[j]){
+            return false
+        }
+    }
+    return true
+    
+    
+    
+}
