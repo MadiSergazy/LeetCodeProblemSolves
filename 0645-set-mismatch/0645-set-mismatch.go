9e0fb1b188e3804ec023ecb8449f9d4c021470ee
MadiSergazy <113364296+MadiSergazy@users.noreply.github.com>
2022-10-23 13:42:04 +0600

    Time: 550 ms (5.55%), Space: 6.6 MB (87.04%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.go
+++ 0645-set-mismatch/0645-set-mismatch.go
@@ -3,26 +3,29 @@ func findErrorNums(nums []int) []int {
    
     dup, missing := -1, -1
     
-    for i := 1; i <= len(nums); i++{
+    for i := 1; i <= len(nums); i++{ //начинаем с того числа который будет в начале массива тоесть 1 и закончиваем длиной потомушто пропущенное число моежт быть в самом конце 
         count := 0
-        for  j := 0; j < len(nums); j++{
+        for j := 0; j < len(nums); j++{
             if nums[j] == i{
                 count++
             }
-            
         }
         if count == 2{
             dup = i
         } else if count == 0{
             missing = i
-        } 
-        if (dup > 0 && missing > 0){
-             ans = append(ans, dup)
+        }
+        if dup > 0 && missing > 0{
+            ans = append(ans, dup)
              ans = append(ans, missing)
             return ans
         }
+        
+        
     }
-   ans = append(ans, dup)
-    ans = append(ans, missing)
-    return ans
+     ans = append(ans, dup)
+             ans = append(ans, missing)
+            return ans
+    
+  
 }
