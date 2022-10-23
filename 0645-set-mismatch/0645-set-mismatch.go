e5fac81a46693737f070f96a8b2bc3e2084ffaa4
MadiSergazy <113364296+MadiSergazy@users.noreply.github.com>
2022-10-23 13:54:48 +0600

    Time: 60 ms (25.93%), Space: 7 MB (35.19%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.go
+++ 0645-set-mismatch/0645-set-mismatch.go
@@ -1,4 +1,53 @@
 func findErrorNums(nums []int) []int {
+    
+	result := make([]int, 2)
+	size := len(nums)
+	sum := 0
+	existNums := make([]bool, size+1)
+	for _, num := range nums {
+		sum += num
+		if !existNums[num] {
+			existNums[num] = true
+			continue
+		}
+		result[0] = num
+	}
+
+	result[1] = size*(size+1)/2 - sum + result[0]
+
+	return result
+}
+    
+   /* var ans []int
+    sort.Ints(nums)
+    dup, missing := -1, -1
+    for i := 1; i < len(nums); i++{
+        if nums[i] == nums[i-1]{
+            dup = nums[i]
+        } else if nums[i] > nums[i-1]+1{
+            missing = nums[i-1]+1
+        }
+    }
+    
+    if nums[len(nums)-1] != len(nums) {
+    ans = append(ans, dup)
+             ans = append(ans, len(nums))
+            return ans
+    } else {
+       
+         ans = append(ans, dup)
+             ans = append(ans, missing)
+            return ans
+            
+    }
+
+    */
+
+
+
+
+
+/*func findErrorNums(nums []int) []int {
     var ans []int
    
     dup, missing := -1, -1
@@ -28,4 +77,4 @@ func findErrorNums(nums []int) []int {
             return ans
     
   
-}
+}*/
