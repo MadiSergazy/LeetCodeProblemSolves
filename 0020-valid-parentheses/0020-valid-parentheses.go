4464bdae92ce4f64abb207cac2adfdfe06449045
MadiSergazy <113364296+MadiSergazy@users.noreply.github.com>
2022-10-30 14:22:18 +0600

    Time: 0 ms (100.00%), Space: 2 MB (88.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.go
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -3,8 +3,29 @@ func isValid(s string) bool {
     for _, v := range s{
         mapa[string(v)]++
     }*/
+    m := map[rune]rune {
+    '(' : ')',
+    '[': ']',
+    '{': '}',
+    }
    
    var q []rune
+    for _, c := range s {
+        switch c {
+            
+    case '[', '{', '(':
+             q = append(q, m[c])
+             default:
+            if len(q) == 0 || c != q[len(q)-1]{
+             return false
+            } else {
+                q = q[:len(q)-1]
+            }
+        }
+        
+    }
+     return len(q)==0
+    /*
 	for _, c := range s {
         switch c {
             
@@ -25,5 +46,5 @@ func isValid(s string) bool {
         }   
     }
     return len(q)==0
-
+*/
 }
